c2183e0ab6637d838af9d4d8d41b5d27f1ea6b70
Amar Singh <ummerr@gmail.com>
2015-01-09 17:30:11 -0500

    able to use Soda gem to get elements

--- sat_nyc.rb
+++ sat_nyc.rb
@@ -5,7 +5,7 @@ my_app_token = 'xQSxQTgxE84h7p1i4byfItWMR'
 
 client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "xQSxQTgxE84h7p1i4byfItWMR"})
 
-puts "Welcome to NYC SAT SCORE searcher."
+puts "Welcome to NYC SCHOOL SAT SCORE searcher."
 puts "Enter a minimum score that you want to search by (between 0 and 800)"
 score = gets.chomp
 puts "Please enter the subject of SAT score you want to search by:"
@@ -15,7 +15,10 @@ subject = gets.chomp
 score_response = client.get("zt9s-n5aj", {'$where' => "#{subject} > #{score}"})
 
 score_response.each do |school|
-  p school.school_name
+  puts "#{school.school_name}"
+  puts "WRITING: #{school.writing_mean}"
+  puts "READING: #{school.critical_reading_mean}"
+  puts "MATHS: #{school.mathematics_mean}"
 end
 
 
--- test.rb
+++ test.rb
@@ -1,22 +1,17 @@
-# api_endpoint = https://data.cityofnewyork.us/resource/zt9s-n5aj.json
 require 'soda'
+require 'pry'
 
 my_app_token = 'xQSxQTgxE84h7p1i4byfItWMR'
 
 client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "xQSxQTgxE84h7p1i4byfItWMR"})
 
-# reading_response = client.get("zt9s-n5aj", {'$where' => "critical_reading_mean > 600"})
-# math_response = client.get("zt9s-n5aj", {'$where' => "mathematics_mean > 600"})
-# writing_response = client.get("zt9s-n5aj", {'$where' => "writing_mean > 600"})
+score_response = client.get("zt9s-n5aj")
 
-score_response = client.get("zt9s-n5aj", {'$where' => "writing_mean > 600", '$where' => "critical_reading_mean > 600", '$where' => "mathematics_mean > 600"})
+score_response.each do |row|
+  row.each do |element|
+    p element
+  end
+end
 
-schools = score_response.each do |school|
-            puts school.school_name
-          end
 
-p schools
 
-# p reading_response
-# p math_response
-# p writing_response
