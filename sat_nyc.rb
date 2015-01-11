a25e37cb54adb8d82a612495a0d3ef5adcf039cb
Amar Singh <ummerr@gmail.com>
2015-01-10 19:17:12 -0500

    made Sat Score Searcher more Object Oriented

--- sat_nyc.rb
+++ sat_nyc.rb
@@ -1,25 +1,58 @@
 require 'soda'
 require 'pry'
 
-my_app_token = 'xQSxQTgxE84h7p1i4byfItWMR'
 
-client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "xQSxQTgxE84h7p1i4byfItWMR"})
+class SatScoreGenerator
 
-puts "Welcome to NYC SCHOOL SAT SCORE searcher."
-puts "Enter a minimum score that you want to search by (between 0 and 800)"
-score = gets.chomp
-puts "Please enter the subject of SAT score you want to search by:"
-puts "Your options are 'critical_reading_mean', 'writing_mean' and 'mathematics_mean'"
-subject = gets.chomp
+  attr_accessor :score, :subject
+  attr_reader :client, :response
 
-score_response = client.get("zt9s-n5aj", {'$where' => "#{subject} > #{score}"})
+  def initialize
+    @client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "xQSxQTgxE84h7p1i4byfItWMR"})
+    @response = client.get("zt9s-n5aj")
+    @score = nil
+    @subject = nil
+    get_score
+  end
 
-score_response.each do |school|
-  puts "#{school.school_name}"
-  puts "WRITING: #{school.writing_mean}"
-  puts "READING: #{school.critical_reading_mean}"
-  puts "MATHS: #{school.mathematics_mean}"
+  def get_score
+    puts "Welcome to NYC SCHOOL SAT SCORE searcher."
+    puts "Enter a minimum score that you want to search by (between 0 and 800)"
+    @score = gets.chomp
+    get_subject
+  end
+
+  def get_subject
+    puts "Please enter the subject of SAT score you want to search by:"
+    puts "The options are 'Math', 'Writing' or 'Reading'"
+    subject = gets.chomp.downcase
+    if subject == 'math'
+      @subject = "mathematics_mean"
+    elsif subject == 'writing'
+      @subject = "writing_mean"
+    elsif subject == 'reading'
+      @subject = "critical_reading_mean"
+    else
+      puts "The options are 'Math', 'Writing' or 'Reading'"
+    end
+    score_response
+  end
+
+  def score_response
+    response = client.get("zt9s-n5aj", {'$where' => "#{@subject} > #{@score}"})
+
+    response.each do |school|
+      puts "#{school.school_name}"
+      puts "WRITING: #{school.writing_mean}"
+      puts "READING: #{school.critical_reading_mean}"
+      puts "MATHS: #{school.mathematics_mean}"
+    end
+  end
+
 end
+
+sat = SatScoreGenerator.new
+
 
 
 
--- test.rb
+++ test.rb
@@ -5,7 +5,19 @@ my_app_token = 'xQSxQTgxE84h7p1i4byfItWMR'
 
 client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "xQSxQTgxE84h7p1i4byfItWMR"})
 
-score_response = client.get("zt9s-n5aj")
+
+class SatScoreGenerator
+
+  def initialize
+    @client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "xQSxQTgxE84h7p1i4byfItWMR"})
+    @response = client.get("zt9s-n5aj")
+  end
+
+
+
+
+end
+
 
 score_response.each do |row|
   row.each do |element|
